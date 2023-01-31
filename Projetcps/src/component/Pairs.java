be0d01a4b536ac66ee8fb3f70e5e3808341029ea
giugiu <61977659+giugiu-stella@users.noreply.github.com>
2023-01-31 20:51:01 +0100

    first commit

---
+++ Projetcps/src/component/Pairs.java
@@ -0,0 +1,10 @@
+package component;
+
+import fr.sorbonne_u.components.AbstractComponent;
+
+public class Pairs extends AbstractComponent {
+	protected Pairs(int nbT, int nbST) {
+		super(nbT,nbST);
+	}
+	//def internes à définir
+}
---
+++ Projetcps/src/interfaces/ContentNodeAddressI.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface ContentNodeAddressI {
+
+}
