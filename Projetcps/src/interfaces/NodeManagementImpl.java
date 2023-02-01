d7e3cd46de136545e48c869428919d1940e07b92
Waly97 <114082936+Waly97@users.noreply.github.com>
2023-02-01 20:24:35 +0100

    Update NodeManagementImpl.java

--- Projetcps/src/interfaces/NodeManagementImpl.java
+++ Projetcps/src/interfaces/NodeManagementImpl.java
@@ -1,5 +1,8 @@
 package interfaces;
 
-public interface NodeManagementImpl {
+import java.util.Set;
 
+public interface NodeManagementImpl {
+	public Set<PeerNodeAddressI> join(PeerNodeAddressI a);
+	public void leave(PeerNodeAddressI a);
 }
