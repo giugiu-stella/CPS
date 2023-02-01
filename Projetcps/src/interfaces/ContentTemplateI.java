b449f78ff3b239dc452991425a02d12c31fcc3e2
Waly97 <114082936+Waly97@users.noreply.github.com>
2023-02-01 20:24:25 +0100

    Update ContentTemplateI.java

--- Projetcps/src/interfaces/ContentTemplateI.java
+++ Projetcps/src/interfaces/ContentTemplateI.java
@@ -1,5 +1,11 @@
 package interfaces;
 
-public interface ContentTemplateI {
+import java.util.Set;
 
+public interface ContentTemplateI {
+	public String getTitle();
+	public String getAlbunmTitle();
+	public Set<String> getInterpreters();
+	public Set<String> getComposers();
+	
 }
