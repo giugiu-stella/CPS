a5b5724a1a6218c892700cb1c1e75941b7e6512f
Waly97 <114082936+Waly97@users.noreply.github.com>
2023-02-01 20:24:15 +0100

    Update ContentDescriptorI.java

--- Projetcps/src/interfaces/ContentDescriptorI.java
+++ Projetcps/src/interfaces/ContentDescriptorI.java
@@ -1,5 +1,9 @@
 package interfaces;
 
-public interface ContentDescriptorI {
+public interface ContentDescriptorI extends ContentTemplateI{
+	public ContentNodeAddressI getContentNodesAddress();
+	public long getSize();
+	public boolean equals(ContentDescriptorI cd);
+	public boolean match(ContentTemplateI t);
 
 }
